f2403d696ac19587ac910b72b6d9a80d4f315842
motiurrahman124 <motiurr503@gmail.com>
2023-01-18 16:50:59 +0600

    css apply

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Calculator from './components/Calculator';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
-import User from './components/User';
-import Todo from './components/Todo';
-import ClockList from './components/ClockList';
-import Bracket from './components/composition/Bracket';
-import Emoji from './components/composition/Emoji';
-import Text from './components/composition/Text';
-import Form from './components/Form';
+import Calculator from "./components/Calculator";
+import ClickCounter from "./components/ClickCounter";
+import ClockList from "./components/ClockList";
+import Bracket from "./components/composition/Bracket";
+import Emoji from "./components/composition/Emoji";
+import Text from "./components/composition/Text";
+import Form from "./components/Form";
+import HoverCounter from "./components/HoverCounter";
+import Logo from "./components/Logo";
+import Todo from "./components/Todo";
+import User from "./components/User";
+
+//link with external css file
+import "./assets/css/global.css";
 // function App() {
 //   return (
 //     <h1 className="heading">
@@ -23,35 +27,42 @@ function Welcome(props) {
 function App() {
   const quantities = [1, 2, 3];
   return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
-      <Welcome name="Hary" />
-      <ClockList quantities={quantities}/>
-      <Form/>
-      <Calculator/>
-      {/* <Emoji>
+    <>
+      <div>
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+        <Welcome name="Hary" />
+        <ClockList quantities={quantities} />
+        <Form />
+        <Calculator />
+        {/* <Emoji>
         {({addEmoji})=><Text addEmoji={addEmoji}/>}
       </Emoji> */}
-      <Emoji>
-            {({ addEmoji }) => (
-                <Bracket>
-                    {({ addBracket }) => <Text addEmoji={addEmoji} addBracket={addBracket} />}
-                </Bracket>
-            )}
-      </Emoji><br />
-      <ClickCounter/><br />
-      <HoverCounter/><br />
-      <User name={(isLoggedIn) => (isLoggedIn ? 'Motiur': 'Guest')}/>
-      <Todo/>
-
-    </div>
+        <Emoji>
+          {({ addEmoji }) => (
+            <Bracket>
+              {({ addBracket }) => (
+                <Text addEmoji={addEmoji} addBracket={addBracket} />
+              )}
+            </Bracket>
+          )}
+        </Emoji>
+        <br />
+        <ClickCounter />
+        <br />
+        <HoverCounter />
+        <br />
+        <User name={(isLoggedIn) => (isLoggedIn ? "Motiur" : "Guest")} />
+        <Todo />
+      </div>
+      <br /><br />
+      <div className="App">
+        <h1>Css Styles Practice</h1>
+        <Logo/>
+      </div>
+    </>
   );
 }
-
-
-
-
 
 export default App;
